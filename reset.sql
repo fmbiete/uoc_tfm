33e4f75849bf7fb693138ef9d8e575eae015c7fb
Francisco Miguel Biete Banon <fbiete@uoc.edu>
2023-10-15 19:20:21 +0100

    user creation

--- reset.sql
+++ reset.sql
@@ -1,3 +1,7 @@
+create database tfm;
+create user tfm with password 'password';
+grant connect on database tfm to tfm;
+\connect tfm
 drop schema tfm;
 create schema tfm;
 alter schema tfm owner to tfm;
